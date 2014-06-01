fb8ac5da0341e3538e636150a10a5e6d0c75fe53
Viktor Ivanov <v.kr.ivanov@gmail.com>
2014-06-01 22:43:19 +0300

    Created function to calculate C(n, k).

--- Utility/Math.py
+++ Utility/Math.py
@@ -1,9 +1,9 @@
+import math
 class Combinatorics:
     @classmethod
     def c(cls, n, k):
         result = 1
         for i in range(n - k + 1, n + 1):
             result *= i
-        for i in range(2, k + 1):
-            result /= i
+        result /= math.factorial(k)
         return result
