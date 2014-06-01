e4c0ef75631f611624321e3c1d2795b7edb002dc
Viktor Ivanov <v.kr.ivanov@gmail.com>
2014-06-01 23:31:10 +0300

    Changed project name.

--- DiscreteDistributions/wsgi.py
+++ DiscreteDistributions/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DiscreteDistributions2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DiscreteDistributions.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
