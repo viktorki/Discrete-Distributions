cd5a9ecd019e4eadd3b503397a669a04d0f633bf
Viktor Ivanov <v.kr.ivanov@gmail.com>
2014-06-21 23:27:12 +0300

    Fixed a problem with data types.

--- Utility/Math.py
+++ Utility/Math.py
@@ -2,6 +2,8 @@ import math
 class Combinatorics:
     @classmethod
     def c(cls, n, k):
+        n = int(n)
+        k = int(k)
         result = 1
         for i in range(n - k + 1, n + 1):
             result *= i
