40e9e11a3831a49319366c09fdca3d56fa952f1f
Viktor Ivanov <v.kr.ivanov@gmail.com>
2014-06-22 19:39:30 +0300

    Added subset generator.

--- Utility/Math.py
+++ Utility/Math.py
@@ -1,4 +1,6 @@
 import math
+import random
+
 class Combinatorics:
     @classmethod
     def c(cls, n, k):
@@ -8,4 +10,16 @@ class Combinatorics:
         for i in range(n - k + 1, n + 1):
             result *= i
         result /= math.factorial(k)
+        return result
+
+class Set:
+    @classmethod
+    def randomSubset(cls, n, m):
+        result = [(i, 0) for i in range(n)]
+        generated = 0
+        while generated < m:
+            position = int(n * random.random())
+            if result[position] == (position, 0):
+                result[position] = (position, 1)
+                generated += 1
         return result
