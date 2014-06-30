76d5856ace0b975f42eed03bd5119907ec380573
Viktor Ivanov <v.kr.ivanov@gmail.com>
2014-07-01 01:06:42 +0300

    Changed DiscreteRandomVariable class.

--- DiscreteRandomVariables/DiscreteRandomVariable.py
+++ DiscreteRandomVariables/DiscreteRandomVariable.py
@@ -1,5 +1,6 @@
-from DiscreteRandomVariables.DiscreteDistribution import *
 import random
+from Utility.Constants import Constants
+
 class DiscreteRandomVariable:
     def __init__(self, distribution):
         self.custom_distribution = distribution.custom
@@ -18,8 +19,21 @@ class DiscreteRandomVariable:
                     return value
         else:
             current = 0
-            while(True):
+            while True:
                 probability_distribution += self.probability_density_function(current)
                 if probability_distribution >= generated:
                     return current
                 current += 1
+
+def getRandomVariable(distribution_id,
+                      distribution_parameter1,
+                      distribution_parameter2,
+                      distribution_parameter3):
+    Distribution = Constants.DISTRIBUTION_BY_ID[distribution_id]
+    if Distribution.parameter_count == 1:
+        return DiscreteRandomVariable(Distribution(distribution_parameter1))
+    elif Distribution.parameter_count == 2:
+        return DiscreteRandomVariable(Distribution(distribution_parameter1, distribution_parameter2))
+    else:
+        return DiscreteRandomVariable(Distribution(distribution_parameter1, distribution_parameter2, distribution_parameter3))
+    
