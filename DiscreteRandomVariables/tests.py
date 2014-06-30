2cce3231d5d887e66d9df26fe1da5cdff498a1ce
Viktor Ivanov <v.kr.ivanov@gmail.com>
2014-07-01 01:07:38 +0300

    Added test.

--- DiscreteRandomVariables/tests.py
+++ DiscreteRandomVariables/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PollsViewsTestCase(TestCase):
+    def test_index(self):
+        resp = self.client.get("/add_problem/")
+        self.assertEqual(resp.status_code, 200)
