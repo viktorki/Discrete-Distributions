39ee1f77c98d9635df040977ade1a7c529f03480
Viktor Ivanov <v.kr.ivanov@gmail.com>
2014-07-01 01:07:04 +0300

    Changed Problem model.

--- DiscreteRandomVariables/models.py
+++ DiscreteRandomVariables/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Problem(models.Model):
-    problem_title = models.CharField(max_length = 128)
+    problem_title = models.CharField(max_length=128)
     problem_statement = models.TextField()
-    solution = models.TextField()
+    solution = models.TextField(blank=True)
     distribution_id = models.IntegerField()
     distribution_parameter1 = models.FloatField()
     distribution_parameter2 = models.FloatField()
